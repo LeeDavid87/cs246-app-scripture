7db251daf53973139a8e5c39e71e6f41c6c18c10
LeeDavid87 <david.silva.lee@gmail.com>
2015-05-20 15:41:51 -0600

    First App for cs246

---
+++ app/src/main/java/com/example/david/cs246_scripture/DisplayScriptureActivity.java
@@ -0,0 +1,45 @@
+package com.example.david.cs246_scripture;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class DisplayScriptureActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(ScriptureActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView displayScript = new TextView(this);
+        displayScript.setTextSize(18);
+        displayScript.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(displayScript);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
